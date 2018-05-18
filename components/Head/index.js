dd0446e5f817bb1385b286b61c139690b179afb4
lokuhra <leandro.88.bernardi@gmail.com>
2018-05-17 21:05:55 -0300

    commit

--- components/Head/index.js
+++ components/Head/index.js
@@ -6,9 +6,6 @@ export default ({ title }) => (
     <title>{title}</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css" />
-    <link rel="stylesheet" href="../../static/style.css" />
-    <link rel="stylesheet" href="../../static/propeller.css" />
-    {/*<link rel="stylesheet" href="../../static/propeller-material.css" />*/}
+    <link rel="stylesheet" href="../../static/styles.css" />
   </Head>
 );
